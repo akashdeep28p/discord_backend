ae1fa5a500b66e47d236c4d254e044b40d92e8ed
Akash <akashyap@lenovo.com>
2022-03-11 23:17:59 +0530

    Added register and login functionality

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,10 +1,45 @@
+const userModel = require('../models/user')
+const bcrypt = require('bcryptjs')
+const {StatusCodes} = require('http-status-codes')
 
 const login = async (req, res) => {
-    res.send('login route')
+    try {
+        const {email, password} = req.body
+
+        const user = await userModel.findOne({email: email.toLowerCase()})
+
+        if (!user) return res.status(StatusCodes.UNAUTHORIZED).send('User does not exist')
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) return res.status(StatusCodes.UNAUTHORIZED).send('Invalid credentials')
+
+        res.status(StatusCodes.OK).json({user})
+    }catch (err){
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Something went wrong...Please try again')
+    }
 }
 
 const register = async (req, res) => {
-    res.send('register route')
+    try {
+        const {name, email, password} = req.body
+
+        const userExists = await userModel.exists({email: email.toLowerCase()})
+
+        if (userExists) return res.status(StatusCodes.CONFLICT).send('email already in use')
+
+        const encryptedPassword = await bcrypt.hash(password, 10)
+
+        const user = await userModel.create({
+            name,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+        })
+
+        res.status(StatusCodes.CREATED).json({user})
+    }catch (err){
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Something went wrong...Please try again')
+    }
 }
 
 module.exports = {
