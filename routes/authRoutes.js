7a5a069084ac01d1771523f8ee8a727379fcd9e4
Akash <akashyap@lenovo.com>
2022-03-11 21:58:59 +0530

    Added request validation with joi

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+const joi = require('joi')
+const validator = require('express-joi-validation').createValidator({})
+
+const registerSchema = joi.object({
+    name : joi.string().min(3).max(12).required(),
+    email : joi.string().email().required(),
+    password : joi.string().min(6).required()
+})
+
+const loginSchema = joi.object({
+    email : joi.string().email().required(),
+    password : joi.string().min(6).required()
+})
+
 const authControllers = require('../controllers/authControllers')
+const Joi = require("joi");
 
-router.post('/register', authControllers.register)
+router.post('/register', validator.body(registerSchema), authControllers.register)
 
-router.post('/login', authControllers.login)
+router.post('/login', validator.body(loginSchema), authControllers.login)
 
 module.exports = router
